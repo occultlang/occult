8476c87b60133b986f1cc56d06de3e5bbdddf20f
final <63569156+finalxvd@users.noreply.github.com>
2024-11-09 22:57:53 -0500

    Organized source

---
+++ src/lexer/lexer.hpp
@@ -0,0 +1,130 @@
+#pragma once
+#include <bit>
+#include <cctype>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <print>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <stack>
+
+namespace occult {
+  enum token_type {
+    // whitespace + comments
+    whitespace_tt,
+    comment_tt,
+    multiline_comment_tt,
+
+    // literals
+    number_literal_tt,
+    string_literal_tt,
+    char_literal_tt,
+    float_literal_tt,
+
+    // arithmetic operators
+    add_operator_tt,      // + 
+    subtract_operator_tt, // - 
+    unary_plus_operator_tt,      // + unary
+    unary_minus_operator_tt, // - unary
+    
+    multiply_operator_tt, // *
+    division_operator_tt, // /
+    modulo_operator_tt,   // %
+    
+    bitwise_and_tt, // &
+    unary_bitwise_not_tt, // ~
+    bitwise_or_tt, // |
+    xor_operator_tt, // ^
+    bitwise_lshift_tt, // <<
+    bitwise_rshift_tt, // >>
+    
+    // logical operators
+    and_operator_tt, // &&
+    or_operator_tt,  // ||
+    unary_not_operator_tt, // ! unary
+
+    // relational operators
+    equals_operator_tt,                // ==
+    not_equals_operator_tt,            // !=
+    greater_than_operator_tt,          // >
+    less_than_operator_tt,             // <
+    greater_than_or_equal_operator_tt, // >=
+    less_than_or_equal_operator_tt,    // <=
+
+    // assignment with pseudocode
+    assignment_tt, // var name = value;
+
+    // delimiters
+    right_paren_tt,
+    left_paren_tt,
+    right_bracket_tt,
+    left_bracket_tt,
+    right_curly_bracket_tt,
+    left_curly_bracket_tt,
+    semicolon_tt,
+    comma_tt,
+
+    // identifier + keywords
+    identifier_tt,       // anything else that is not a keyword or in this list
+    function_keyword_tt, // fn
+    if_keyword_tt,       // if
+    else_keyword_tt,     // else
+    elseif_keyword_tt,   // elif
+    loop_keyword_tt,     // loop
+    return_keyword_tt,   // return
+    break_keyword_tt,    // break
+    continue_keyword_tt, // continue
+    while_keyword_tt,    // while
+    for_keyword_tt,      // for
+    include_keyword_tt,  // include
+    int64_keyword_tt,    // int64
+    int32_keyword_tt,    // int32
+    int16_keyword_tt,    // int16
+    int8_keyword_tt,     // int8
+    uint64_keyword_tt,   // uint64
+    uint32_keyword_tt,   // uint32
+    uint16_keyword_tt,   // uint16
+    uint8_keyword_tt,    // uint8
+    float32_keyword_tt,  // float32
+    float64_keyword_tt,  // float64
+    boolean_keyword_tt,  // bool,
+    char_keyword_tt,     // char
+    string_keyword_tt,   // str
+    true_keyword_tt,     // true
+    false_keyword_tt,    // false
+
+    end_of_file_tt,
+    unkown_tt,
+    function_call_parser_tt,
+    argument_count_tt,
+  };
+
+  typedef struct token_t {
+    std::uintptr_t line;
+    std::uintptr_t column;
+    std::string lexeme;
+    token_type tt;
+
+    static std::string get_typename(token_type tt);
+
+    token_t(std::uintptr_t line, std::uintptr_t column, std::string lexeme, token_type tt) : line(line), column(column), lexeme(lexeme), tt(tt) {}
+  } token_t; // token structure
+
+  class lexer {
+    std::string source;
+    std::uintptr_t pos;
+    std::uintptr_t line;
+    std::uintptr_t column;
+    
+    std::vector<token_t> stream;
+    token_t get_next();             // getting next token to put into stream
+  public:
+    lexer(std::string source) : source(source), pos(0), line(1), column(1) {}
+    
+    std::vector<token_t> analyze(); // returns a token stream which will be put into the parser later on
+    void visualize(std::optional<std::vector<token_t>> o_s = std::nullopt); 
+  };
+} // namespace occult
---
+++ src/parser/ast.hpp
@@ -0,0 +1,184 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "../lexer/lexer.hpp"
+
+namespace occult {
+  enum class ast_type {
+    root,
+    binaryexpr,
+    block,
+    identifier,
+    function,
+    ifstmt,
+    elsestmt,
+    elseifstmt,
+    loopstmt,
+    whilestmt,
+    forstmt,
+    continuestmt,
+    breakstmt,
+    returnstmt,
+    functionarguments,
+    functioncall,
+    assignment,
+    int8_datatype, // char as well as boolean
+    int16_datatype,
+    int32_datatype,
+    int64_datatype,
+    uint8_datatype,
+    uint16_datatype,
+    uint32_datatype,
+    uint64_datatype,
+    float32_datatype,
+    float64_datatype,
+    string_datatype,
+    number_literal,
+    float_literal,
+    add_operator,
+    subtract_operator,
+    unary_plus_operator,
+    unary_minus_operator,
+    multiply_operator,
+    division_operator,
+    modulo_operator,
+    bitwise_and,
+    unary_bitwise_not,
+    bitwise_or,
+    xor_operator,
+    bitwise_lshift,
+    bitwise_rshift,
+    and_operator,
+    or_operator,
+    unary_not_operator,
+    equals_operator,
+    not_equals_operator,
+    greater_than_operator,
+    less_than_operator,
+    greater_than_or_equal,
+    less_than_or_equal,
+    greater_than_or_equal_operator,
+    less_than_or_equal_operator,
+    callarg,
+    comma,
+    include,
+    stringliteral,
+    charliteral
+  };
+  
+  class ast {
+    std::vector<std::unique_ptr<ast>> children;
+  public:
+    ast() = default;
+    
+    std::string content = ""; // base class
+    
+    template<typename BaseAst = ast>
+    static std::unique_ptr<BaseAst> new_node() {
+      return std::make_unique<BaseAst>();
+    }
+    
+    template<typename BaseAst = ast>
+    static std::unique_ptr<BaseAst> new_node(std::string content) {
+      auto node = std::make_unique<BaseAst>();
+      node->content = content;
+      
+      return node;
+    }
+    
+    void add_child(std::unique_ptr<ast> child) {
+        children.push_back(std::move(child));
+    }
+    
+    std::vector<std::unique_ptr<ast>>& get_children() {
+        return children;
+    }
+    
+    virtual ast_type get_type() = 0;
+    
+    virtual std::string to_string() = 0;
+    
+    void visualize(int depth = 0) {
+        std::string indent(depth * 2, ' ');
+        std::print("{}", indent + to_string());
+        
+        if (!content.empty())
+          std::print(": {}\n", content);
+        else
+          std::println();
+        
+        for (const auto& child : children) {
+            child->visualize(depth + 1);
+        }
+    }
+  };
+  
+  #define NODE(tt, nn)  \
+  class nn : public ast { \
+    public: \
+    ast_type get_type() override { \
+      return ast_type::tt; \
+    } \
+    std::string to_string() override { \
+      return #nn; \
+    } \
+  };
+  
+  NODE(root, ast_root)
+  NODE(binaryexpr, ast_binaryexpr)
+  NODE(block, ast_block)
+  NODE(identifier, ast_identifier)
+  NODE(function, ast_function)
+  NODE(functionarguments, ast_functionargs)
+  NODE(callarg, ast_callarg)
+  NODE(functioncall, ast_functioncall)
+  NODE(ifstmt, ast_ifstmt)
+  NODE(elsestmt, ast_elsestmt)
+  NODE(elseifstmt, ast_elseifstmt)
+  NODE(loopstmt, ast_loopstmt)
+  NODE(whilestmt, ast_whilestmt)
+  NODE(forstmt, ast_forstmt)
+  NODE(continuestmt, ast_continuestmt)
+  NODE(breakstmt, ast_breakstmt)
+  NODE(returnstmt, ast_returnstmt)
+  NODE(assignment, ast_assignment)
+  NODE(int8_datatype, ast_int8)
+  NODE(int16_datatype, ast_int16)
+  NODE(int32_datatype, ast_int32)
+  NODE(int64_datatype, ast_int64)
+  NODE(uint8_datatype, ast_uint8)
+  NODE(uint16_datatype, ast_uint16)
+  NODE(uint32_datatype, ast_uint32)
+  NODE(uint64_datatype, ast_uint64)
+  NODE(float32_datatype, ast_float32)
+  NODE(float64_datatype, ast_float64)
+  NODE(string_datatype, ast_string)
+  NODE(number_literal, ast_numberliteral)
+  NODE(float_literal, ast_floatliteral)
+  NODE(add_operator, ast_add)
+  NODE(subtract_operator, ast_subtract)
+  NODE(unary_plus_operator, ast_unary_plus)
+  NODE(unary_minus_operator, ast_unary_minus)
+  NODE(multiply_operator, ast_multiply)
+  NODE(division_operator, ast_divide)
+  NODE(modulo_operator, ast_modulo)
+  NODE(bitwise_and, ast_bitwise_and)
+  NODE(unary_bitwise_not, ast_unary_bitwise_not)
+  NODE(bitwise_or, ast_bitwise_or)
+  NODE(xor_operator, ast_xor)
+  NODE(bitwise_lshift, ast_bitwise_lshift)
+  NODE(bitwise_rshift, ast_bitwise_rshift)
+  NODE(and_operator, ast_and)
+  NODE(or_operator, ast_or)
+  NODE(unary_not_operator, ast_unary_not)
+  NODE(equals_operator, ast_equals)
+  NODE(not_equals_operator, ast_not_equals)
+  NODE(greater_than_operator, ast_greater_than)
+  NODE(less_than_operator, ast_less_than)
+  NODE(greater_than_or_equal_operator, ast_greater_than_or_equal)
+  NODE(less_than_or_equal_operator, ast_less_than_or_equal)
+  NODE(comma, ast_comma)
+  NODE(include, ast_includestmt)
+  NODE(stringliteral, ast_stringliteral)
+  NODE(charliteral, ast_charliteral)
+} // namespace occult
